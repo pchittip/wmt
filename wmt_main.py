ee668261431b236abaf6c6f71fa8b2eec51adc8e
stkyle <steve.kyle@gmail.com>
2014-04-11 19:50:38 -0400

    comit

--- wmt_main.py
+++ wmt_main.py
@@ -23,3 +23,32 @@ import os
 #print os.path.dirname(__file__)
 #print os.path._getfullpathname(__file__)
 
+## start by looking at the different store types
+#wmt_learn.stores.sort_index(by=['Size'],inplace=True)
+
+
+plt.close('all')
+fig = plt.figure()
+#fig
+ax1 = fig.add_subplot(111)
+store_sizes=wmt_learn.stores['Size']
+
+wmt_learn.stores.Store.values
+
+bars = ax1.bar(range(1,46), store_sizes, color='blue', edgecolor='black')
+
+#pull out the A stores and make them red
+Astores = wmt_learn.stores[wmt_learn.stores['Type']=='A']
+for store_num in Astores['Store']:
+    bars[store_num-1].set_facecolor('red')
+
+
+#pull out the B stores and make them green
+Bstores = wmt_learn.stores[wmt_learn.stores['Type']=='B']
+for store_num in Bstores['Store']:
+    bars[store_num-1].set_facecolor('green')
+
+    
+    
+#plt.xticks(range(1,45))
+plt.show()
